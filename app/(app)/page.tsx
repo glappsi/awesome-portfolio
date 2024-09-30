972a1d3036b45551c165c2a7d2ba90135192b267
Florian Glappa <glappa.js@gmail.com>
2024-09-30 13:52:47 +0200

    feat: landing page

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -2,6 +2,17 @@ import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
 import { getHeroPage } from '@/actions';
+import { HomeIcon, CubeIcon, GitHubLogoIcon } from "@radix-ui/react-icons"
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Dock, DockIcon } from "@/components/ui/dock";
 
 const navigation = [
   { name: "Projects", href: "/projects" },
@@ -10,53 +21,101 @@ const navigation = [
 
 export const revalidate = 60;
 
+const DATA = {
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/projects", icon: CubeIcon, label: "Projects" },
+  ],
+  contact: {
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/glappsi",
+        icon: GitHubLogoIcon,
+      }
+    },
+  },
+};
+
 export default async function Home() {
   const heroPage = await getHeroPage();
 
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <nav className="my-16 animate-fade-in">
-        <ul className="flex items-center justify-center gap-4">
-          {navigation.map((item) => (
-            <Link
-              key={item.href}
-              href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-            >
-              {item.name}
-            </Link>
-          ))}
-        </ul>
-      </nav>
+
+      {heroPage.hero && (
+        <img
+          src={heroPage.hero.url}
+          alt={heroPage.hero.alt}
+          className="max-h-[200px] animate-fade-in rounded-full border p-1"
+        />
+      )}
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      {heroPage.hero && (
-        <img
-          src={heroPage.hero.url}
-          alt={heroPage.hero.alt}
-          className="h-96 animate-fade-in"
-        />
-      )}
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+
+      <h1 className="py-8 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
         {heroPage.title}
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in">
+      <div className="my-8 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+        With over 10 years of expertise in frontend development, I create intuitive, high-performance user interfaces<br />that seamlessly blend design and functionality, driving impactful digital experiences.
         </h2>
       </div>
+
+      <nav className="animate-fade-in">
+        <TooltipProvider>
+          <Dock direction="middle">
+            {DATA.navbar.map((item) => (
+              <DockIcon key={item.label}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Link
+                      href={item.href}
+                      aria-label={item.label}
+                      className={cn(
+                        buttonVariants({ variant: "ghost", size: "icon" }),
+                        "size-12 rounded-full",
+                      )}
+                    >
+                      <item.icon className="size-4" />
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{item.label}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </DockIcon>
+            ))}
+            <Separator orientation="vertical" className="h-full" />
+            {Object.entries(DATA.contact.social).map(([name, social]) => (
+              <DockIcon key={name}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Link
+                      href={social.url}
+                      aria-label={social.name}
+                      className={cn(
+                        buttonVariants({ variant: "ghost", size: "icon" }),
+                        "size-12 rounded-full",
+                      )}
+                    >
+                      <social.icon className="size-4" />
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{name}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </DockIcon>
+            ))}
+          </Dock>
+        </TooltipProvider>
+      </nav>
     </div>
   );
 
