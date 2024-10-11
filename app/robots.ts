3b44167d774ec14ce6f38dfe1a8cb6662f5c4e78
Florian Glappa <glappa.js@gmail.com>
2024-10-11 23:43:03 +0200

    chore: add robots

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { url } from '@/lib/env'
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/admin/'],
+    },
+    sitemap: `${url}/sitemap.xml`,
+  }
+}
