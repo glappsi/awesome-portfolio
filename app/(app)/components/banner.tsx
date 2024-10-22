ebf36f97355b4771f403311a1bb9687099b39b00
Florian Glappa <glappa.js@gmail.com>
2024-10-22 02:04:44 +0200

    chore: remove coffee script

--- app/(app)/components/banner.tsx
+++ app/(app)/components/banner.tsx
@@ -42,20 +42,6 @@ export function Banner({
             alt="Buy Me A Coffee"
             style={{ height: 35, width: 126.6 }} />
         </a>
-        <script
-          type="text/javascript"
-          src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
-          data-name="bmc-button"
-          data-slug="glapps"
-          data-color="#FFDD00"
-          data-emoji=""
-          data-font="Cookie"
-          data-text="Buy me a coffee"
-          data-outline-color="#000000"
-          data-font-color="#000000"
-          data-coffee-color="#ffffff"
-          async>
-        </script>
       </div>
     </div>
   )
