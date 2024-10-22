9ea458c5e9742076cd4ebaae76b6682bdc9f48d2
Florian Glappa <glappa.js@gmail.com>
2024-10-22 02:08:11 +0200

    chore: do not shrink bmc button

--- app/(app)/components/banner.tsx
+++ app/(app)/components/banner.tsx
@@ -36,7 +36,8 @@ export function Banner({
 
         <a
           href="https://www.buymeacoffee.com/glapps"
-          target="_blank">
+          target="_blank"
+          className='shrink-0'>
           <img
             src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
             alt="Buy Me A Coffee"
